4286acb87148d9b18f23ab09639ecfc3f29a925b
Simon Repp <simon@fdpl.io>
2018-06-22 16:00:24 +0200

    Evaluate expanded benchmarks for ruby

--- benchmark.rb
+++ benchmark.rb
@@ -36,18 +36,33 @@ class RubyReport
   end
 
   def generate
-    scenario('yaml_invoice_example')
+    scenario('abstract_hierarchy')
 
-    eno_invoice = File.read('samples/yaml_invoice_example/invoice.eno')
-    toml_invoice = File.read('samples/yaml_invoice_example/invoice.toml')
-    yaml_invoice = File.read('samples/yaml_invoice_example/invoice.yaml')
+    eno_hierarchy = File.read('samples/abstract_hierarchy/hierarchy.eno')
+    toml_hierarchy = File.read('samples/abstract_hierarchy/hierarchy.toml')
+    yaml_hierarchy = File.read('samples/abstract_hierarchy/hierarchy.yaml')
 
-    benchmark('enorb', ENORB_VERSION) { Eno.parse(eno_invoice) }
-    # benchmark('safe_yaml', SAFE_YAML_VERSION) { SafeYAML.load(yaml_invoice) }
-    # benchmark('toml', TOML_VERSION) { TOML.load(toml_invoice) } ERRORS
-    benchmark('toml-rb', TOML_RB_VERSION) { TomlRB.parse(toml_invoice) }
-    benchmark('tomlrb', TOMLRB_VERSION) { Tomlrb.parse(toml_invoice) }
-    benchmark('yaml', YAML_VERSION) { YAML.load(yaml_invoice) }
+    benchmark('enorb', ENORB_VERSION) { Eno.parse(eno_hierarchy) }
+    # benchmark('safe_yaml', SAFE_YAML_VERSION) { SafeYAML.load(yaml_hierarchy) }
+    benchmark('toml', TOML_VERSION) { TOML.load(toml_hierarchy) }
+    benchmark('toml-rb', TOML_RB_VERSION) { TomlRB.parse(toml_hierarchy) }
+    benchmark('tomlrb', TOMLRB_VERSION) { Tomlrb.parse(toml_hierarchy) }
+    benchmark('yaml', YAML_VERSION) { YAML.load(yaml_hierarchy) }
+
+
+    scenario('invented_server_configuration')
+
+    eno_configuration = File.read('samples/invented_server_configuration/configuration.eno')
+    toml_configuration = File.read('samples/invented_server_configuration/configuration.toml')
+    yaml_configuration = File.read('samples/invented_server_configuration/configuration.yaml')
+
+    benchmark('enorb', ENORB_VERSION) { Eno.parse(eno_configuration) }
+    # benchmark('safe_yaml', SAFE_YAML_VERSION) { SafeYAML.load(yaml_configuration) }
+    benchmark('toml', TOML_VERSION) { TOML.load(toml_configuration) }
+    benchmark('toml-rb', TOML_RB_VERSION) { TomlRB.parse(toml_configuration) }
+    benchmark('tomlrb', TOMLRB_VERSION) { Tomlrb.parse(toml_configuration) }
+    benchmark('yaml', YAML_VERSION) { YAML.load(yaml_configuration) }
+
 
     scenario('jekyll_post_example')
 
@@ -61,6 +76,34 @@ class RubyReport
     benchmark('toml-rb', TOML_RB_VERSION) { TomlRB.parse(toml_post) }
     benchmark('tomlrb', TOMLRB_VERSION) { Tomlrb.parse(toml_post) }
     benchmark('yaml', YAML_VERSION) { YAML.load(yaml_post) }
+
+
+    scenario('journey_route_data')
+
+    eno_journey = File.read('samples/journey_route_data/journey.eno')
+    toml_journey = File.read('samples/journey_route_data/journey.toml')
+    yaml_journey = File.read('samples/journey_route_data/journey.yaml')
+
+    benchmark('enorb', ENORB_VERSION) { Eno.parse(eno_journey) }
+    # benchmark('safe_yaml', SAFE_YAML_VERSION) { SafeYAML.load(yaml_journey) }
+    benchmark('toml', TOML_VERSION) { TOML.load(toml_journey) }
+    benchmark('toml-rb', TOML_RB_VERSION) { TomlRB.parse(toml_journey) }
+    benchmark('tomlrb', TOMLRB_VERSION) { Tomlrb.parse(toml_journey) }
+    benchmark('yaml', YAML_VERSION) { YAML.load(yaml_journey) }
+
+
+    scenario('yaml_invoice_example')
+
+    eno_invoice = File.read('samples/yaml_invoice_example/invoice.eno')
+    toml_invoice = File.read('samples/yaml_invoice_example/invoice.toml')
+    yaml_invoice = File.read('samples/yaml_invoice_example/invoice.yaml')
+
+    benchmark('enorb', ENORB_VERSION) { Eno.parse(eno_invoice) }
+    # benchmark('safe_yaml', SAFE_YAML_VERSION) { SafeYAML.load(yaml_invoice) }
+    # benchmark('toml', TOML_VERSION) { TOML.load(toml_invoice) }
+    benchmark('toml-rb', TOML_RB_VERSION) { TomlRB.parse(toml_invoice) }
+    benchmark('tomlrb', TOMLRB_VERSION) { Tomlrb.parse(toml_invoice) }
+    benchmark('yaml', YAML_VERSION) { YAML.load(yaml_invoice) }
 
     File.write('reports/ruby.eno', @report)
   end
