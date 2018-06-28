84fe8f3f438b9496891b6524c7a7768a4f9176d6
Simon Repp <simon@fdpl.io>
2018-06-28 17:08:56 +0200

    Update report texts and visual scale of all graphs

--- report.js
+++ report.js
@@ -49,29 +49,57 @@ for(language of ['javascript', 'python', 'ruby']) {
 let report = '# Benchmarks\n';
 
 report += `
-This is an initial draft report, and as with all statistics please take it with a big grain of salt.
+These are benchmarks to evaluate the performance of all current eno library
+implementations in comparism to each other, as well as in comparism to the most
+popular yaml/toml parsers out there.
+
+As with all statistics please take these findings with a grain of salt,
+and feel warmly invited to re-run these benchmarks or point out flaws and
+possible improvements to the methodology and code.
+
+## How the data is gathered
 
 To get an impression how the measurements were obtained, please take a look at the source of \`benchmark.js/py/rb\` inside this repository.
 To get an impression how the report was compiled, please study \`report.js\` inside this repository.
 
+Benchmarks are currently performed on Ubuntu 17.10 on an Intel® Xeon(R) CPU E5-1650 v3 @ 3.50GHz × 12 and in recent language runtimes (node 10.5.0, python 3.6.3, ruby 2.5.0p0).
+
+## How to interpret the results below
+
 Numerical values represent the number of seconds elapsed during 100k (\*) iterations of the respective code example, or in other words, smaller numbers indicate better performance.
 
-(\*) Note: Some libraries included in the benchmark exhibit an up to 1000x slower performance speed compared to the top ranking parsers, these have been partially sampled with up to only 1k iterations with the total duration extrapolated for the global comparison again.
+Each ░ represents one second, the maximum bar width displayed in a line is 60 seconds, so this represents one minute in the graphs below:
+
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+(\*) Note: Some libraries included in the benchmark exhibit an up to 1000x
+slower performance speed compared to the top ranking parsers, these have been
+partially sampled with up to only 1k iterations, with the total duration
+extrapolated for the global comparison again. Although they somewhat destroy the
+layout, their graphical bar representations are included in the report
+nonetheless, it should give us some thought that popular libraries with
+downloads going up into the millions exhibit that kind of performance in a
+simple, random benchmark.
+
+## The latest report
+
+Generated on ${new Date()}
+
 `;
 
 for(let scenario of Object.keys(results)) {
-  report += `\n## ${scenario}\n\n`;
+  report += `\n### ${scenario}\n\n`;
 
   for(let benchmark of results[scenario]) {
     const barChar = benchmark.library.includes('eno') ? '▓' : '░';
 
-    let bar = 1 + 320 * (benchmark.time / maxTime);
+    let bar = 60 * (benchmark.time / 60);
 
-    if(bar > 80) {
+    if(bar > 60) {
       report += '\n';
       while(bar > 0) {
-        report += barChar.repeat(Math.min(bar, 80)) + '  \n';
-        bar -= 80;
+        report += barChar.repeat(Math.min(bar, 60)) + '  \n';
+        bar -= 60;
       }
 
       report += `${benchmark.time.toFixed(3)} - **${benchmark.library}** - *${benchmark.language}*  \n`;
